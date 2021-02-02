e03b7c7202d75cf13376389247f70f0556238dd3
v1rt24 <v1rt24@mail.ru>
2021-02-03 01:03:34 +0300

    Финал

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,8 @@ jQuery(document).ready(function () {
 
   let madams;
 
-  // После голосования убираем кнопки и создаём localStorage,
-  // чтобы после обновления страницы голосование было не доступно
   function checkLocalStorage () {
-    if (localStorage.getItem('desc') && localStorage.getItem('desc') ===
-      location.href) {
+    if (localStorage.getItem('desc') && localStorage.getItem('desc') === location.href) {
       jQuery('.cardButton button').remove();
       return true;
     }
@@ -15,19 +12,9 @@ jQuery(document).ready(function () {
 
   checkLocalStorage();
 
-  ////////////////// СОЗДАЁМ СОКЕТ
   let socket = new WebSocket('ws://127.0.0.1:12345');
 
-  // Сработает при подключении
-  socket.onopen = function (e) {
-    // console.log("[open] Соединение установлено");
-    // console.log("Отправляем данные на сервер");
-    // socket.send("Отправляем на сервер сообщение");
-  };
-
-  // Получаем данные с сервера
   socket.onmessage = function (event) {
-    // console.log(`[message] Данные получены с сервера: ${event.data}`);
 
     madams = JSON.parse(event.data);
 
@@ -38,24 +25,9 @@ jQuery(document).ready(function () {
     });
   };
 
-  // Срабатывает при закрытии соединения
-  socket.onclose = function (event) {
-    if (event.wasClean) {
-      console.log(
-        `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
-    }
-    else {
-      // например, сервер убил процесс или сеть недоступна
-      // обычно в этом случае event.code 1006
-      console.log('[close] Соединение прервано');
-    }
-  };
-
-  // Срабатывает при ошибках соединения
   socket.onerror = function (error) {
     console.log(`[error] ${error.message}`);
   };
-  ////////////////// / СОЗДАЁМ СОКЕТ
 
   jQuery('[data-fancybox]').fancybox({
     touch: false,
@@ -78,13 +50,11 @@ jQuery(document).ready(function () {
         madams[name] = {count: '1'};
       }
 
-      // Отправляем данные на сервер
       socket.send(JSON.stringify(madams));
 
-      // Удаляем кнопку и записываем в localStorage
       jQuery('.cardButton button').remove();
       localStorage.setItem('desc', location.href);
     }
   });
 
-});//Конец ready
+});
--- server/server.php
+++ server/server.php
@@ -1,49 +1,32 @@
 <?php
 
 use Workerman\Worker;
-use Workerman\Lib\Timer; // подключаем таймер
+use Workerman\Lib\Timer;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Создание Websocket сервера
 $ws_worker = new Worker( 'websocket://0.0.0.0:12345' );
 
-// 4 processes
 $ws_worker->count = 4;
 
-// Срабатывает при подключении клиента
 $ws_worker->onConnect = function ( $connection ) {
-	echo "Новое подключение\n";
-
-    // Отправляем данные на клиента. Сейчас отправляем с файла, но можно с БД
 	$file = file_get_contents( __DIR__ . '/madams.json' );
 	$connection->send( $file );
 
-	// Запускаем таймер на считывание файла каждую секунду, т.е. если в нём поменяем количество, то на странице оно тоже изменится
-	// \Workerman\Lib\Timer::add(); - так если не подключили через: use Workerman\Lib\Timer;
-	Timer::add( 1, function () use ( $connection ) { // 1 - это поставили 1 секунду
+	Timer::add( 1, function () use ( $connection ) {
 		$file = file_get_contents( __DIR__ . '/madams.json' );
 		$connection->send( $file );
 	} );
 };
 
-// Получаем данные с клиента. Данные находятся в $data
 $ws_worker->onMessage = function ( $connection, $data ) use ( $ws_worker ) {
-	// Записываем пришедшие данные в файл
-	file_put_contents( __DIR__ . '../madams.json', $data );
+	file_put_contents( __DIR__ . '/madams.json', $data );
 
-    // Отправляем полученные данные всем подключенным клиентам, чтобы обновилось у всех
 	foreach ( $ws_worker->connections as $clientConnection ) {
 		$clientConnection->send( $data );
 	}
 };
 
-// Оповещения о закрытии соединения (к примеру, когда клиент закрывает вкладку в браузере)
-$ws_worker->onClose = function ( $connection ) {
-	echo "Соединение закрыто\n";
-};
+$ws_worker->onClose = function ( $connection ) {};
 
-// Запуск worker
 Worker::runAll();
-
-// В консоли запускаем так: php server/server.php / php server/server.php start
